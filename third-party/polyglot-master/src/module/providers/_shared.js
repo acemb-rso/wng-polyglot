db5d79729d1bac826bef9c95d7d80f139741a4a8
acemb-rso <acember@iu.edu>
2025-11-07 17:14:57 -0500

    Delete third-party/polyglot-master directory

--- third-party/polyglot-master/src/module/providers/_shared.js
+++
@@ -1,7 +0,0 @@
-/** Providers whose systems use "-"" in their names */
-export const providerKeys = {
-	"cyberpunk-red-core": "cyberpunkRed",
-	"dark-heresy": "darkHeresy",
-	"draw-steel": "drawSteel",
-	"uesrpg-d100": "uesrpg",
-};
