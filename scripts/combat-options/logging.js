aba86f0f23965d778892d45ed01291e0c5e176e0
acemb-rso <acember@iu.edu>
2025-11-18 16:07:17 -0500

    Route combat extender debug logs through logger

--- scripts/combat-options/logging.js
+++ scripts/combat-options/logging.js
@@ -7,9 +7,14 @@ export const log = (level, message, ...data) => {
 
 export const logError = (...args) => log("error", ...args);
 
-export const isDebugEnabled = () => Boolean(game?.modules?.get?.(MODULE_ID)?.flags?.debug);
+export const isDebugEnabled = () => {
+  const settingsDebug = game?.settings?.get?.(MODULE_ID, "debugLogging");
+  if (typeof settingsDebug === "boolean") return settingsDebug;
+
+  return Boolean(game?.modules?.get?.(MODULE_ID)?.flags?.debug);
+};
 
 export const logDebug = (...args) => {
   if (!isDebugEnabled()) return;
-  log("debug", ...args);
+  log("info", ...args);
 };
