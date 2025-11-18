348a66076f4cc6ae6ff030adbc270e928280c658
acemb-rso <acember@iu.edu>
2025-11-18 14:57:53 -0500

    Add GM-only debug logging setting

--- scripts/combat-options.js
+++ scripts/combat-options.js
@@ -1,6 +1,8 @@
 import { registerEngagementAutomation } from "./combat-options/engagement.js";
+import { registerSettings } from "./combat-options/settings.js";
 import { registerTurnEffectHooks } from "./combat-options/turn-effects.js";
 import "./combat-options/dialog.js";
 
+registerSettings();
 registerEngagementAutomation();
 registerTurnEffectHooks();
